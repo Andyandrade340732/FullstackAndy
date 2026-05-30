3641c156765ed53c987c9c7814f8e1baf4892d65
Andyandrade340732 <xavigamer200@gmail.com>
2026-05-30 20:40:57 -0300

    mas cambios en los schemas

--- src/server/v1/schemas/game.validate.js
+++ src/server/v1/schemas/game.validate.js
@@ -5,7 +5,9 @@ export const schemaBodyJuego = Joi.object({
     titulo: Joi.string().min(3).max(100).required(),
     descripcion: Joi.string().min(3).max(500).required(),
     anioLanzamiento: Joi.number().integer().min(1970).max(2030),
-    categoriaId: Joi.string()
+    categoriaId: Joi.string(),
+    imageUrl: Joi.string(),
+    imagePublicId: Joi.string()
 });
 
 export const schemaParamsJuego = Joi.object({
